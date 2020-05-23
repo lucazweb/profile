34be534bb44285f93e550c7b51c7b5ae8aa19850
Lucas Santos <lucazwebmail@gmail.com>
2020-05-23 15:52:28 -0300

    fix: repolist adjustment

--- src/components/ProjectsList/repolist.component.js
+++ src/components/ProjectsList/repolist.component.js
@@ -1,60 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { ExternalLink } from '../Menu/menu.styled';
-import { RepoList, RepoListItem, RepoTitle, RepoDesc } from './repolist.styled';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { ExternalLink } from "../Menu/menu.styled";
+import { RepoList, RepoListItem, RepoTitle, RepoDesc } from "./repolist.styled";
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://api.github.com'
+  baseURL: "https://api.github.com",
 });
 
 export const ProjectList = () => {
   const [repos, setRepos] = useState([]);
-  
+
   const showRepos = [
-    'transactions-manager-app',
-    'omnistack-react-app',
-    'star-warjs',
-    'spotify-web-browser'
+    "transactions-manager-app",
+    "omnistack-react-app",
+    "hi-beats",
+    "star-warjs",
+    //'profile-react-app',
+    "spotify-web-browser",
   ];
 
   useEffect(() => {
     const handleRequests = async () => {
-     
-      if(repos.length === 0) {
+      if (repos.length === 0) {
         try {
-          const { data } = await api.get('/users/lucazweb/repos')
-          const allowed = data.filter(repo => showRepos.includes(repo.name));
-          console.log(allowed);
+          const { data } = await api.get("/users/lucazweb/repos");
+          const allowed = data.filter((repo) => showRepos.includes(repo.name));
           setRepos(allowed);
-        } catch(error) {
+        } catch (error) {
           console.log(error);
         }
       }
-    } 
+    };
     handleRequests();
-
   }, [setRepos, repos, showRepos]);
 
   return (
     <RepoList>
-      {
-        repos.length > 0
-        ? (
+      {repos.length > 0 ? (
         <React.Fragment>
-          {
-            repos.map((repo, index) => (
-              <ExternalLink href={repo.html_url} target="_blank">
-                <RepoListItem key={index}> 
-                  <RepoTitle>{repo.name}</RepoTitle>
-                  <RepoDesc>{repo.description}</RepoDesc>
-                </RepoListItem>
-              </ExternalLink>
-            ))
-          }
+          {repos.map((repo, index) => (
+            <ExternalLink href={repo.html_url} target="_blank">
+              <RepoListItem key={index}>
+                <RepoTitle>{repo.name}</RepoTitle>
+                <RepoDesc>{repo.description}</RepoDesc>
+              </RepoListItem>
+            </ExternalLink>
+          ))}
         </React.Fragment>
-        )
-        : (<span> Nada a exibir </span>)
-      }
+      ) : (
+        <span> Nada a exibir </span>
+      )}
     </RepoList>
   );
 };
