49f702f2fe9f0c2388fdeadfcb3d09125d3ca6c3
Lucas Santos <lucazwebmail@gmail.com>
2020-12-16 19:34:27 -0300

    Updates

---
+++ precache-manifest.fb23a0500a2ab74e2bde70c570b804dc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157eb873838604e34ac8c098fa95fc5d",
+    "url": "/profile-react-app/index.html"
+  },
+  {
+    "revision": "ba05c617eec50ead506c",
+    "url": "/profile-react-app/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "bac8bcc13841feb55dd3",
+    "url": "/profile-react-app/static/css/main.afb711fe.chunk.css"
+  },
+  {
+    "revision": "ba05c617eec50ead506c",
+    "url": "/profile-react-app/static/js/2.87d144d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile-react-app/static/js/2.87d144d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac8bcc13841feb55dd3",
+    "url": "/profile-react-app/static/js/main.d2eeb9c0.chunk.js"
+  },
+  {
+    "revision": "00cc3c2e0436d44b6f19",
+    "url": "/profile-react-app/static/js/runtime-main.938466e7.js"
+  },
+  {
+    "revision": "7eebf6c52745468430948ab252afe97b",
+    "url": "/profile-react-app/static/media/lucas_santos_front_end_cv.7eebf6c5.pdf"
+  },
+  {
+    "revision": "6074efc7bb46afdb596ab3d976b5ee93",
+    "url": "/profile-react-app/static/media/lucas_santos_resume.6074efc7.pdf"
+  },
+  {
+    "revision": "0a6724de08260863ae936aebb882a518",
+    "url": "/profile-react-app/static/media/lucazweb.0a6724de.jpeg"
+  }
+]);
